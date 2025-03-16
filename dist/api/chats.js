562cabbf9670e55046401fd3e0dbabeeca5e1b67
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2025-03-16 18:27:02 -0300

    Add AI thread creation to chat retrieval logic

--- dist/api/chats.js
+++ dist/api/chats.js
@@ -2,11 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Chat = void 0;
 const client_1 = require("./client");
+const ai_1 = require("../ai");
 exports.Chat = {
     status: () => client_1.client.get("/chats/instance/status"),
     qrcode: () => client_1.client.get("/chats/instance/qr-code"),
     list: async () => client_1.client.get("/chats"),
-    get: async (id) => client_1.client.get(`/chats?refer_id=${id}`).then(({ data }) => data?.items?.[0]),
+    get: async (id) => {
+        const chat = await client_1.client.get(`/chats?refer_id=${id}`).then(({ data }) => data?.items?.[0]);
+        if (!!chat && !!chat.id)
+            return chat;
+        console.log(`==== [${id}] Creating OpenAI thread`);
+        const threadId = await ai_1.openai.thread.create(id);
+        console.log(`==== [${id}] Creating contact on Naturals`);
+        const { data } = await client_1.client.post(`/chats`, {
+            ai_enabled: true,
+            refer_id: id,
+            thread_id: threadId,
+        });
+        return data;
+    },
     create: async (data) => client_1.client.post(`/chats`, data),
     update: Object.assign(async (id, data) => client_1.client.put(`/chats/${id}`, data), {
         agent: async (id) => client_1.client.put(`/chats/${id}/ai`),
--- src/api/chats.ts
+++ src/api/chats.ts
@@ -1,4 +1,5 @@
 import { client } from "./client";
+import { openai } from "../ai";
 
 export interface CreateChatParams {
   refer_id: string; // <WA_ID>
@@ -11,7 +12,23 @@ export const Chat = {
   status: () => client.get("/chats/instance/status") as any,
   qrcode: () => client.get("/chats/instance/qr-code") as any,
   list: async () => client.get("/chats") as any,
-  get: async (id: string) => client.get(`/chats?refer_id=${id}`).then(({ data }: any) => data?.items?.[0]) as any,
+  get: async (id: string) => {
+    const chat = await client.get(`/chats?refer_id=${id}`).then(({ data }: any) => data?.items?.[0]);
+
+    if (!!chat && !!chat.id) return chat;
+
+    console.log(`==== [${id}] Creating OpenAI thread`);
+    const threadId = await openai.thread.create(id);
+
+    console.log(`==== [${id}] Creating contact on Naturals`);
+    const { data } = await client.post(`/chats`, {
+      ai_enabled: true,
+      refer_id: id,
+      thread_id: threadId,
+    }) as any;
+
+    return data;
+  },
   create: async (data: CreateChatParams) => client.post(`/chats`, data) as any,
   update: Object.assign(
     async (id: string, data: any) => client.put(`/chats/${id}`, data) as any,
